b2578d00bec36bc07ba7b7393ac608f3b2fe7193
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2011-01-20 02:28:28 +0100

    default view rendering, layout view

--- test/fixtures/mymodule/controllers/mycontroller.js
+++ test/fixtures/mymodule/controllers/mycontroller.js
@@ -1,3 +1,7 @@
 exports.myaction = function(req, res) {
     res.send('test passed');
 };
+
+exports.getview = function(req, res) {
+    res.render();
+};
--- test/test.js
+++ test/test.js
@@ -1,31 +1,50 @@
-test('setup', 1, function() {
-    var handler,
-        app = {
-            get: function(route, _handler){
-                handler = _handler;    
-            },
-            post: function(){},
-            put: function(){},
-            del: function(){}
-        },
-        req = {
-            params: {
-                module: 'mymodule',
-                controller: 'mycontroller',
-                action: 'myaction'
-            }    
-        },
-        res = {
-            send: function(body) {
-                equal(body, 'test passed', 'action was called')
-            }
-        };
-    
-    var options = {
-        root: __dirname + '/fixtures'    
-    };
-    
-    setup(app, options);
-    
-    handler(req, res);
+var express = require('express'),
+    http = require('http');
+
+var o = {
+        root: __dirname + '/fixtures',
+        port: 8888    
+    },
+    app;
+
+
+function request(path, callback) {
+    var client = http.createClient(o.port);
+    var request = client.request('GET', path, {'host': 'http://localhost'});
+    request.end();
+    request.on('response', function (response) {
+        response.setEncoding('utf8');
+	    response.on('data', callback);
+    });    
+}
+
+QUnit.module('mvc', {
+    setup: function() {
+        app = express.createServer();
+        app.set('view engine', 'html');
+        app.register('.html', require('jqtpl'));        
+        setup(app, o);
+        app.listen(o.port);
+    },
+    teardown: function() {
+         app.close();       
+    }
 });
+
+test('action call', 1, function() {
+    stop();
+
+    request('/mymodule/mycontroller/myaction', function(data) {
+        equal(data, 'test passed', 'action call works');
+        start();
+    });
+});
+
+test('default view', 1, function() {
+    stop();
+    request('/mymodule/mycontroller/getview', function(data) {
+        equal(data, 'hello world', 'default view rendered');
+        start();
+    });
+});
+
